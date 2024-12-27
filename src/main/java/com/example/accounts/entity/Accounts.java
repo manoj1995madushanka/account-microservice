21e8f988fc58bff2f1b0be11432e98fec7a6814b
manoj1995madushanka <madushankamanoj414@gmail.com>
2024-12-27 11:20:25 +0530

    fix entity class

--- src/main/java/com/example/accounts/entity/Accounts.java
+++ src/main/java/com/example/accounts/entity/Accounts.java
@@ -1,11 +1,13 @@
 package com.example.accounts.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class Accounts extends BaseEntity {
     @Id
--- src/main/java/com/example/accounts/entity/Customer.java
+++ src/main/java/com/example/accounts/entity/Customer.java
@@ -1,11 +1,13 @@
 package com.example.accounts.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class Customer extends BaseEntity {
     @Id
