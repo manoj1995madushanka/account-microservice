892c82f776849ea5cc57c772304ea090db9a442c
manoj1995madushanka <madushankamanoj414@gmail.com>
2024-12-29 16:44:56 +0530

    implement crud endpoints

---
+++ src/main/java/com/example/accounts/constants/AccountConstants.java
@@ -0,0 +1,17 @@
+package com.example.accounts.constants;
+
+public class AccountConstants {
+
+    private AccountConstants() {
+
+    }
+
+    public static final String SAVINGS = "Savings";
+    public static final String ADDRESS = "123 Main Street,Galle.";
+    public static final String STATUS_201 = "201";
+    public static final String STATUS_200 = "200";
+    public static final String MESSAGE_201 = "Account Created";
+    public static final String MESSAGE_200 = "Request Succeed";
+    public static final String STATUS_500 = "500";
+    public static final String MESSAGE_500 = "An error occurred.";
+}
--- src/main/java/com/example/accounts/controller/AccountsController.java
+++ src/main/java/com/example/accounts/controller/AccountsController.java
@@ -1,13 +1,62 @@
 package com.example.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.accounts.constants.AccountConstants;
+import com.example.accounts.dto.CustomerDto;
+import com.example.accounts.dto.ResponseDto;
+import com.example.accounts.service.IAccountService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
 public class AccountsController {
 
-    @GetMapping("sayHello")
-    public String sayHello(){
-        return "say hello";
+    private IAccountService accountService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+        accountService.createAccount(customerDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountConstants.STATUS_201, AccountConstants.MESSAGE_201));
+    }
+
+    @GetMapping("/fetch")
+    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber) {
+        CustomerDto customerDto = accountService.fetchAccount(mobileNumber);
+        return ResponseEntity.status(HttpStatus.OK).body(customerDto);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateAccountDetails(@RequestBody CustomerDto customerDto) {
+        boolean isUpdated = accountService.updateAccount(customerDto);
+        if (isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountConstants.STATUS_200, AccountConstants.MESSAGE_200));
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(AccountConstants.STATUS_500, AccountConstants.MESSAGE_500));
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam String mobileNumber) {
+        boolean isDeleted = accountService.deleteAccount(mobileNumber);
+        if (isDeleted) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountConstants.STATUS_200, AccountConstants.MESSAGE_200));
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDto(AccountConstants.STATUS_500, AccountConstants.MESSAGE_500));
+        }
     }
 }
